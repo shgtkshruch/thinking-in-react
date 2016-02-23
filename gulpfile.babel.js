cb3f8226d92fb2cb4781bd6e3cea91fba33ccc64
shgtkshruch <shgtk.shruch@gmail.com>
2016-02-23 14:45:56 +0900

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import browserSync from 'browser-sync';
+
+const reload = browserSync.reload;
+
+gulp.task('browserSync', () => {
+  browserSync({
+    server: {
+      baseDir: 'dest'
+    },
+    browser: 'google chrome canary',
+    notify: false
+  });
+});
+
+gulp.task('html', () => {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dest'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('javascript', () => {
+  return gulp.src('src/scripts/app.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dest/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['html', 'javascript', 'browserSync'], () => {
+  gulp.watch('src/index.html', ['html']);
+  gulp.watch('src/scripts/app.js', ['javascript']);
+});

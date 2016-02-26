2be00fef8a19aff0aa860fbcff72a5927b6e3e3d
shgtkshruch <shgtk.shruch@gmail.com>
2016-02-26 09:14:18 +0900

    Add inverse data flow

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -56,12 +56,30 @@ var ProductTable = React.createClass({
 });
 
 var SearchBar = React.createClass({
+  handleChange() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.value,
+      this.refs.inStockOnlyInput.checked
+    );
+  },
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Search..." value={this.props.filterText}/>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange}
+        />
         <p>
-          <input type="checkbox" checked={this.props.inStockOnly} />
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            ref="inStockOnlyInput"
+            onChange={this.handleChange}
+          />
           {' '}
           Only show products in stock
         </p>
@@ -78,12 +96,20 @@ var FilterableProductTable = React.createClass({
     }
   },
 
+  handleUserInput(filterText, inStockOnly) {
+    this.setState({
+      filterText: filterText,
+      inStockOnly: inStockOnly
+    });
+  },
+
   render() {
     return (
       <div>
         <SearchBar
           filterText={this.state.filterText}
           inStockOnly={this.state.inStockOnly}
+          onUserInput={this.handleUserInput}
         />
         <ProductTable
           products={this.props.products}
